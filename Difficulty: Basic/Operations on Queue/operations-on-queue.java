b5ef4eff15815e5bc1d215e51c2b37834a2b5abe
ChinmaipavaniDasari <165761756+ChinmaipavaniDasari@users.noreply.github.com>
2025-05-30 14:14:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Operations on Queue/operations-on-queue.java
@@ -0,0 +1,37 @@
+// User function Template for Java
+
+class Solution {
+
+    // Function to push an element in queue.
+    void enqueue(Queue<Integer> q, int x) {
+        q.add(x);
+        
+    }
+
+    // Function to remove front element from queue.
+    void dequeue(Queue<Integer> q) {
+         if(!q.isEmpty()){
+            q.poll();
+        }
+    }
+
+    // Function to find the front element of queue.
+        
+    int front(Queue<Integer> q) {
+          if(!q.isEmpty()){
+            return q.peek();
+        }
+        return -1;
+    }
+
+        
+    // Function to find an element in the queue.
+    String find(Queue<Integer> q, int x) {
+         if(q.contains(x)){
+            return "Yes";
+        }
+        else{
+            return "No";
+        }
+    }
+}
