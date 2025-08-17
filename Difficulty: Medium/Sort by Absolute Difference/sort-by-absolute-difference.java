a88d1944299d64c94622d005ce6149c80cbd0c9f
ChinmaipavaniDasari <165761756+ChinmaipavaniDasari@users.noreply.github.com>
2025-08-17 18:52:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort by Absolute Difference/sort-by-absolute-difference.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void rearrange(int[] arr, int x) {
+          int n = arr.length;
+        int[] temp = new int[n];
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
+            if (a[0] == b[0])
+                return a[1] - b[1];
+            return a[0] - b[0];
+        }); //{diff, i}
+        
+        for (int i = 0; i < n; ++i) {
+            temp[i] = arr[i];
+            int diff = Math.abs(arr[i] - x);
+            pq.offer (new int[] {diff, i});
+        }
+        
+        int idx = 0;
+        while (!pq.isEmpty()) {
+            int id = pq.peek()[1];
+            pq.poll();
+            arr[idx++] = temp[id];
+        }
+    }
+}
