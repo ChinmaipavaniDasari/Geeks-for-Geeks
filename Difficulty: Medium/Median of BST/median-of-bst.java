6bbd0592ec9d45717ae61f6c6398bc0f6292a390
ChinmaipavaniDasari <165761756+ChinmaipavaniDasari@users.noreply.github.com>
2025-10-18 16:08:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Median of BST/median-of-bst.java
@@ -0,0 +1,32 @@
+/*
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+
+class Solution {
+    public int findMedian(Node root) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        helper(root, ans);
+        Collections.sort(ans);
+        int n = ans.size();
+        int idx = (n + 1) / 2;
+        return ans.get(idx-1);
+    }
+    
+    public static void helper(Node root, ArrayList<Integer> ans) {
+        if (root == null) return;
+        ans.add(root.data);
+        helper(root.left, ans);
+        helper(root.right, ans);
+    
+    }
+}
