d0139a6ffa0f6fe02952b9a8f0d88d10d885aa04
ChinmaipavaniDasari <165761756+ChinmaipavaniDasari@users.noreply.github.com>
2025-11-26 08:27:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/AND In Range/and-in-range.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int andInRange(int l, int r) {
+        // Code here
+        int shift = 0;
+
+ 
+
+        // find common prefix
+
+        while (l < r) {
+
+            l >>= 1;
+
+            r >>= 1;
+
+            shift++;
+
+        }
+
+ 
+
+        // restore shifted bits
+
+        return l << shift;
+    }
+}
