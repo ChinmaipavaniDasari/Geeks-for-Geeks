b03a708ac96dc1245c50d98bfa6be41eefaff48e
ChinmaipavaniDasari <165761756+ChinmaipavaniDasari@users.noreply.github.com>
2025-12-10 21:25:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing And Repeating/missing-and-repeating.java
+++ Difficulty: Easy/Missing And Repeating/missing-and-repeating.java
@@ -1,65 +1,22 @@
-//{ Driver Code Starts
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList; // Import ArrayList
-import java.util.Stack;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int t = Integer.parseInt(br.readLine());
-
-        while (t-- > 0) {
-            String input = br.readLine();
-            String[] inputs = input.split(" ");
-            int[] arr = new int[inputs.length];
-
-            for (int i = 0; i < inputs.length; i++) {
-                arr[i] = Integer.parseInt(inputs[i]);
-            }
-
-            // Get the result as an ArrayList<Integer>
-            ArrayList<Integer> ans = new Solution().findTwoElement(arr);
-            System.out.println(ans.get(0) + " " + ans.get(1));
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-
-
 class Solution {
-    // Function to find two elements in array
     ArrayList<Integer> findTwoElement(int arr[]) {
-        int n = arr.length;
-        int repeating = -1;
-        
-        for(int  i = 0;i<n;i++){
-            int val = Math.abs(arr[i]);
-            
-            if(arr[val - 1] > 0){
-                arr[val - 1] = -arr[val-1];
-            }
-            else{
-                repeating = val;
-            }
-        }
-        int missing = -1;
-        for(int i =0;i<n;i++){
-            if(arr[i]>0){
-                missing = i+1;
-                break;
-            }
+        // code here
+         int[] n = new int[arr.length+1];
+        ArrayList<Integer> al = new ArrayList<>();
+        for(int i=0; i<arr.length;i++){
+            n[arr[i]]++;
         }
         
-        ArrayList<Integer> result = new ArrayList<>();
-        result.add(repeating);
-        result.add(missing);
-        return result;
+        for(int i=1;i<n.length;i++){
+            if(n[i]==2){
+                al.add(i);
+            }
+        }
+        for(int i=1;i<n.length;i++){
+            if(n[i]==0){
+                al.add(i);
+            }
+        }
+        return al;
     }
 }
