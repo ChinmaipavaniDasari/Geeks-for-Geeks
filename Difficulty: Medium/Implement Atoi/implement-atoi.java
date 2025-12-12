b1c8acfb0c4037da48c46c9adc1846936aefeb26
ChinmaipavaniDasari <165761756+ChinmaipavaniDasari@users.noreply.github.com>
2025-12-12 12:04:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Implement Atoi/implement-atoi.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int myAtoi(String s) {
+        int i = 0, n = s.length();
+        int sign = 1;
+        long result = 0;
+
+        // 1. Skip leading spaces
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        // 2. Check sign
+        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+            sign = (s.charAt(i) == '-') ? -1 : 1;
+            i++;
+        }
+
+        // 3. Convert digits
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            int digit = s.charAt(i) - '0';
+
+            // 4. Check overflow BEFORE adding
+            if (result > (Integer.MAX_VALUE - digit) / 10) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+
+            result = result * 10 + digit;
+            i++;
+        }
+
+        // 5. Final result with sign
+        return (int)(sign * result);
+    }
+}
