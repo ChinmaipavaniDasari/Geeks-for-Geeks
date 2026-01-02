c92f1a1a2e4986eb25804dc41017df206c19ef59
ChinmaipavaniDasari <165761756+ChinmaipavaniDasari@users.noreply.github.com>
2026-01-02 10:49:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find H-Index/find-hindex.java
+++ Difficulty: Medium/Find H-Index/find-hindex.java
@@ -1,26 +1,22 @@
 class Solution {
     public int hIndex(int[] citations) {
         // code here
-          int n = citations.length;
-        int[] count = new int[n + 1];  
-        
-       
-        for (int c : citations) {
-            if (c >= n) {
-                count[n]++;  
-            } else {
-                count[c]++;
+        int n = citations.length;
+        int[] freq = new int[n+1]; 
+        for(int i = 0;i<n;i++){
+            if(citations[i] >= n){
+                freq[n] = freq[n] + 1;
+            }
+            else{
+                freq[citations[i]]=freq[citations[i]] + 1;
             }
         }
-        
-        int total = 0;
-        for (int i = n; i >= 0; i--) {
-            total += count[i];
-            if (total >= i) {
-                return i;
-            }
+        int idx = n;
+        int s = freq[n];
+        while(s<idx){
+            idx = idx - 1;
+            s = s + freq[idx];
         }
-        
-        return 0;
+        return idx;
     }
 }
