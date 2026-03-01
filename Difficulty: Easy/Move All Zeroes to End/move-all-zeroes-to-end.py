bd8f5448f9bcb57f7b7fae5418d4d69a06460063
ChinmaipavaniDasari <165761756+ChinmaipavaniDasari@users.noreply.github.com>
2026-03-01 17:56:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Move All Zeroes to End/move-all-zeroes-to-end.py
@@ -0,0 +1,13 @@
+class Solution:
+	def pushZerosToEnd(self, arr):
+    	# code here
+    	n = len(arr)
+        j = 0
+   
+        # traverse the array
+        for i in range(n):
+           if arr[i] != 0:
+              arr[j], arr[i] = arr[i], arr[j]
+              j += 1
+            
+        return arr
